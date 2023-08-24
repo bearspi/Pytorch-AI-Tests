a67b865d69e1822171d02e75fba23800f7e0e54e
bearspi <bearsquaduzgun@gmail.com>
2023-08-25 02:53:28 +0300

    modified:   main.py 	new file:   notes.txt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import torch
+import time
 
-scalar = torch.tensor(7)
-vector = torch.tensor([9, 0])
-matrix = torch.tensor([[9, 5], [3, 2]])
-tensor = torch.tensor([[[7, 0], [5, 4]],[[6, 9], [3, 1]]])
-randtensor = torch.rand(4, 2, 3)
-print(randtensor)
+y = torch.rand(size=(1,1,2), device="mps", requires_grad=False)
+print(y / 10)

770416601ef6e2ec2c3b344b689f2f5b5beacc71
bearspi <bearsquaduzgun@gmail.com>
2023-08-25 21:04:45 +0300

    modified:   main.py 	modified:   notes.md

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 import torch
-import time
+from torch import nn
+import matplotlib.pyplot as plt
 
-y = torch.rand(size=(4, 3, 2), device="mps", requires_grad=False)
-x = torch.rand(size=(3, 2, 3), device="mps", requires_grad=False)
+mps_device = torch.device('mps:0' if torch.has_mps else 'cpu')
 
-print(torch.matmul(x, y))
+
+
+weigth = 0.6
+bias = 0.9
+
+start = 0
+end = 1
+step = 0.02
+
+X = torch.arange(start=start, end=end, step=step).unsqueeze(1)
+
+y = weigth * X + bias
+
+train_split = int(0.8 * len(X))
+
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+def plot_predictions(train_data = X_train, train_labels = y_train, test_data = X_test, test_labels = y_test, predictions = None):
+    plt.figure(figsize=(10,7))
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+    plt.scatter(test_data, test_labels, c="y", s=4, label="Testing data")
+    
+    if predictions is not None:
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+    plt.legend(prop={"size": 14});
+    plt.show()
+plot_predictions();
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.weights = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
--- notes.md
+++ notes.md
@@ -1,6 +1,6 @@
 Running on MPS is abou % 130 faster than CPU
  
-Matrix Mult requires reversed *dimensions*:
+Matrix Mult requires reversed **dimensions**:
 
 (3,2) @ (3,2) ❌
 
@@ -8,8 +8,12 @@ Matrix Mult requires reversed *dimensions*:
 
 (2,3) @ (3,2) ✅
 
-Matrix Mult has the shape of the outer *dimensions*:
+Matrix Mult has the shape of the outer **dimensions**:
 
 (3,2) @ (2,3) -> (3,3)
 
 (2,3) @ (3,2) -> (2,2)
+
+When converting 'NumPy arrays' -> 'PyTorch Tensors' it cames as **64 Bit** values.
+
+Numpy cant work on datas on MPS (GPU).

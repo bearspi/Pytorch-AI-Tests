4b8d5060bbf4235e53c2cdc073ddd8171bbb35ee
bearspi <bearsquaduzgun@gmail.com>
2023-08-26 02:25:14 +0300

    Start

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ mps_device = torch.device('mps:0' if torch.has_mps else 'cpu')
 
 
 
-weigth = 0.6
-bias = 0.9
+weigth = 0.69
+bias = 0.31 
 
 start = 0
 end = 1
 step = 0.02
+
+epochs = 200
+
+
 
 X = torch.arange(start=start, end=end, step=step).unsqueeze(1)
 
@@ -31,7 +35,7 @@ def plot_predictions(train_data = X_train, train_labels = y_train, test_data = X
         plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
     plt.legend(prop={"size": 14});
     plt.show()
-plot_predictions();
+
 
 class LinearRegressionModel(nn.Module):
     def __init__(self):
@@ -40,3 +44,31 @@ class LinearRegressionModel(nn.Module):
         self.bias = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.weights * x + self.bias
+
+torch.manual_seed(81)
+model_0 = LinearRegressionModel()
+
+loss_fn = nn.L1Loss()
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.01)
+
+for epoch in range(epochs):
+    model_0.train()
+    y_pred = model_0(X_train)
+    
+    loss = loss_fn(y_pred, y_train)
+    
+    optimizer.zero_grad()
+    
+    loss.backward()
+    
+    optimizer.step()
+    print(f"Loss: {loss}")
+
+with torch.inference_mode():
+    plot_predictions(predictions=model_0(X_test))
+
+
+
+
+
+
